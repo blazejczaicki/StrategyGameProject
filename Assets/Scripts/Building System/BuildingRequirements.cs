fe7ad663fabe0eac7cea8f28b1ba1cbbae1a0799
Blazej <sentai74@gmail.com>
2019-12-18 13:37:07 +0100

    another repairing scriptableObject

--- Assets/Scripts/Building System/BuildingRequirements.cs
+++ Assets/Scripts/Building System/BuildingRequirements.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 [CreateAssetMenu(fileName = "New Building Requirement")]
 public class BuildingRequirements : ScriptableObject
 {
